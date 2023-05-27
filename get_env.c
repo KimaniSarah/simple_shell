c793fec33682b293d11aba6d8c8966202a97ad06
Maryanemwende <maryanemwende333@gmail.com>
2023-05-26 18:39:02 -0700

    Simple Shell

--- get_env.c
+++ get_env.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _getenv - gets environment
- * @name:
+ * @name: env name
  * Return:the env
  */
 char *_getenv(char *name)
@@ -9,11 +9,11 @@ char *_getenv(char *name)
 	int len = _strlen(name);
 	char **env_var = environ;
 
-	while(env_var != NULL)
+	while (env_var != NULL)
 	{
-		if(_strncmp(name, *env_var, len) == 0 && (*env_var)[len] == '=')
+		if (_strncmp(name, *env_var, len) == 0 && (*env_var)[len] == '=')
 		{
-			return &((*env_var)[len + 1]);
+			return (&((*env_var)[len + 1]));
 		}
 		env_var++;
 	}
